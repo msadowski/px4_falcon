faba8c6f8b38a75e99eac3ff30bbd1cf384f6770
Mateusz Sadowski <msadowski90@gmail.com>
2017-02-21 06:17:35 +0100

    Add dummy NED to body rotation for groundspeed command

--- scripts/joy_translator.py
+++ scripts/joy_translator.py
@@ -3,8 +3,11 @@
 import rospy
 from sensor_msgs.msg import Joy 
 from geometry_msgs.msg import TwistStamped
+from std_msgs.msg import Float64
+from math import sqrt,sin,cos,acos,pi
 
 twist_pub = rospy.Publisher('falcon/twist', TwistStamped, queue_size=10)
+heading = 0.0
 
 def joy_callback(msg):    
     x = msg.axes[2]
@@ -12,18 +15,32 @@ def joy_callback(msg):
     z = msg.axes[1]
     rot_z = msg.axes[4]
     
+    v = sqrt(x*x+y*y)
+    if v != 0.0:
+        alpha = acos(x/v)
+    else:
+        alpha = 0.0
+    
+    x = v*cos(heading+alpha)
+    y = v*sin(heading+alpha)
+    rospy.loginfo("v: %f\nhdg: %f\n alpha: %f\n x: %f\n y: %f", v, heading, alpha, x, y)
 
     twist = TwistStamped()
     twist.header.stamp = rospy.Time.now()
-    twist.twist.linear.x = x
-    twist.twist.linear.y = y
+    twist.twist.linear.x = v*cos(heading+alpha)
+    twist.twist.linear.y = v*sin(heading+alpha)
     twist.twist.linear.z = z
     twist.twist.angular.z = rot_z
     twist_pub.publish(twist)
+    
+def hdg_callback(msg):
+    global heading 
+    heading = msg.data*pi/180.0
 
 def main():
     rospy.init_node('joy_translator', anonymous=False)
     rospy.Subscriber('joy', Joy, joy_callback)
+    rospy.Subscriber('mavros/global_position/compass_hdg', Float64, hdg_callback)
     rospy.spin() 
 
 
